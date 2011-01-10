a08f7fe3445efff6f38f2ad01231d48cd54bbe45
Daniel Süpke <suepke@gmx.de>
2011-01-10 11:37:20 +0100

    Added missing or wrongly formatted GPL texts

--- src/de/feanor/yeoldemensa/MenuItem.java
+++ src/de/feanor/yeoldemensa/MenuItem.java
@@ -1,3 +1,22 @@
+/**
+ *   Ye Olde Mensa is an android application for displaying the current
+ *   mensa plans of University Oldenburg on an android mobile phone.
+ *   
+ *   Copyright (C) 2009/2010 Daniel Süpke
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.feanor.yeoldemensa;
 
 import de.feanor.yeoldemensa.Mensa.Day;
