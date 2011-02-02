5f47a9d2d4b25c74ab96faac44ec099f09f002ac
Daniel Süpke <suepke@gmx.de>
2011-02-02 09:58:14 +0100

    Re-added connection timeout

--- src/de/feanor/htmltokenizer/SimpleHTMLTokenizer.java
+++ src/de/feanor/htmltokenizer/SimpleHTMLTokenizer.java
@@ -28,63 +28,82 @@ public class SimpleHTMLTokenizer {
 	private ListIterator<Element> elements;
 
 	public SimpleHTMLTokenizer(URL url, String encoding) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(
-				url.openStream(), encoding));
+		URLConnection conn = url.openConnection();
+		conn.setConnectTimeout(TIMEOUT * 1000);
+		conn.setReadTimeout(TIMEOUT * 1000);
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(conn
+				.getInputStream(), encoding));
 		parse(br);
 		elements = elementList.listIterator();
 	}
-	
-	public SimpleHTMLTokenizer(String urlString, String encoding, String date) throws IOException {
-		
-		     URL url = new URL(urlString);
-		     URLConnection conn = url.openConnection();
-		     HttpURLConnection httpConn = (HttpURLConnection)conn;
-		     
-		     httpConn.setRequestMethod("GET");
-		     httpConn.setDoOutput(true); 
-		     httpConn.setDoInput(true);
-		     httpConn.connect(); 
-		     
-		     BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-		     String line, name, value;
-		     String dataset = URLEncoder.encode("ctl00%24ctl00%24_PlaceHolderLeftNavBarTop%24SearchBox%24S5F437507_InputKeywords", "UTF-8")+"=";
-		     dataset += "&" + URLEncoder.encode("__EVENTTARGET", "UTF-8") + "=" + URLEncoder.encode("ctl00$ctl00$_PlaceHolderCalendarNavigator$PlaceHolderCalendarNavigator$MensaKalender", "UTF-8");
-		     dataset += "&" + URLEncoder.encode("__EVENTARGUMENT", "UTF-8") + "=" + URLEncoder.encode(date, "UTF-8");
-		     while ((line = br.readLine()) != null) {
-		         if(line.matches("^<input.*")){
-		        	 name = line.split(" ")[2].split("=")[1]; 
-		        	 name = name.substring(1, name.length()-1);
-		        	 value = line.split(" ")[4].split("=")[1];
-		        	 
-		        	 if(line.split(" ")[4].split("=").length > 2){
-		        		 //System.out.println(""+line.split(" ")[4].split("=")[2]);
-		        		 value += "="+line.split(" ")[4].split("=")[2];
-		        	 }
-		        	 value = value.substring(1, value.length()-1);
-		        	 //System.out.println(name +": "+ value);
-		        	 dataset += "&" + URLEncoder.encode(name, "UTF-8") + "=" + URLEncoder.encode(value, "UTF-8");
-		        	    	 
-		         }
-		     }
-		     br.close();
-		  
-		     //System.out.println(dataset);
-		     
-		  	 url = new URL(urlString); 
-		     conn = url.openConnection();
-		     
-		     conn.setDoOutput(true);
-		     OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
-		     wr.write(dataset);
-		     wr.flush();
 
-		     // Get the response
-		     br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-		     parse(br);
-		     wr.close();
-		     br.close();
-		 
-		     elements = elementList.listIterator();
+	public SimpleHTMLTokenizer(String urlString, String encoding, String date)
+			throws IOException {
+		URL url = new URL(urlString);
+		URLConnection conn = url.openConnection();
+		conn.setConnectTimeout(TIMEOUT * 1000);
+		conn.setReadTimeout(TIMEOUT * 1000);
+		HttpURLConnection httpConn = (HttpURLConnection) conn;
+
+		httpConn.setRequestMethod("GET");
+		httpConn.setDoOutput(true);
+		httpConn.setDoInput(true);
+		httpConn.connect();
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(conn
+				.getInputStream()));
+		String line, name, value;
+		String dataset = URLEncoder
+				.encode(
+						"ctl00%24ctl00%24_PlaceHolderLeftNavBarTop%24SearchBox%24S5F437507_InputKeywords",
+						"UTF-8")
+				+ "=";
+		dataset += "&"
+				+ URLEncoder.encode("__EVENTTARGET", "UTF-8")
+				+ "="
+				+ URLEncoder
+						.encode(
+								"ctl00$ctl00$_PlaceHolderCalendarNavigator$PlaceHolderCalendarNavigator$MensaKalender",
+								"UTF-8");
+		dataset += "&" + URLEncoder.encode("__EVENTARGUMENT", "UTF-8") + "="
+				+ URLEncoder.encode(date, "UTF-8");
+		while ((line = br.readLine()) != null) {
+			if (line.matches("^<input.*")) {
+				name = line.split(" ")[2].split("=")[1];
+				name = name.substring(1, name.length() - 1);
+				value = line.split(" ")[4].split("=")[1];
+
+				if (line.split(" ")[4].split("=").length > 2) {
+					// System.out.println(""+line.split(" ")[4].split("=")[2]);
+					value += "=" + line.split(" ")[4].split("=")[2];
+				}
+				value = value.substring(1, value.length() - 1);
+				// System.out.println(name +": "+ value);
+				dataset += "&" + URLEncoder.encode(name, "UTF-8") + "="
+						+ URLEncoder.encode(value, "UTF-8");
+
+			}
+		}
+		br.close();
+
+		// System.out.println(dataset);
+
+		url = new URL(urlString);
+		conn = url.openConnection();
+
+		conn.setDoOutput(true);
+		OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+		wr.write(dataset);
+		wr.flush();
+
+		// Get the response
+		br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		parse(br);
+		wr.close();
+		br.close();
+
+		elements = elementList.listIterator();
 	}
 
 	// TODO: This parser is quite bad. After each tag it always assumes a text,
@@ -123,7 +142,7 @@ public class SimpleHTMLTokenizer {
 				}
 
 				// Remove attributes
-				for (int i = 0; i < size-1; i++) {
+				for (int i = 0; i < size - 1; i++) {
 					if (buf[i] == ' ')
 						size = i;
 				}
